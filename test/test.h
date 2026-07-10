6e496b11b2c7696a777c75f5efffa91057e0d0f7
Emma Labbé <emma@labbe.me>
2026-07-09 22:49:45 -0400

    Add `test` command

---
+++ test/test.h
@@ -0,0 +1,19 @@
+//
+//  test.h
+//  test
+//
+//  Created by Emma on 17-06-24.
+//  Copyright © 2024 Emma Labbé. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test.
+FOUNDATION_EXPORT double testVersionNumber;
+
+//! Project version string for test.
+FOUNDATION_EXPORT const unsigned char testVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test/PublicHeader.h>
+
+
